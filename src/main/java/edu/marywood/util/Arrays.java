4e004d53d787e971ee71d627b9c593064c3e8f7a
William DiNoia <william@dinoia.email>
2026-09-06 20:19:04 -0400

    Create Arrays.toString(boolean[]).

--- src/main/java/edu/marywood/util/Arrays.java
+++ src/main/java/edu/marywood/util/Arrays.java
@@ -11,4 +11,23 @@ public class Arrays {
     private Arrays() {
         throw new UnsupportedOperationException();
     }
+    /**
+     * Converts a {@code boolean[]} to a {@link String}.
+     *
+     * @param booleanArray {@code boolean[]}
+     * @return {@link String}
+     */
+    public static String toString(boolean[] booleanArray) {
+        String string;
+        if (booleanArray == null) {
+            string = "null";
+        } else {
+            String[] stringArray = new String[booleanArray.length];
+            for (int index = 0; index < booleanArray.length; index += 1) {
+                stringArray[index] = Boolean.toString(booleanArray[index]);
+            }
+            string = "[" + String.join(", ", stringArray) + "]";
+        }
+        return string;
+    }
 }
