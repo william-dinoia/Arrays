42739107ba1ee8530ac7e8e1ddbda32951879279
William DiNoia <william@dinoia.email>
2026-09-08 19:22:16 -0400

    Create ArraysTest.toStringBooleanArray.

--- src/test/java/edu/marywood/util/ArraysTest.java
+++ src/test/java/edu/marywood/util/ArraysTest.java
@@ -11,4 +11,19 @@ import org.junit.jupiter.api.Test;
 public class ArraysTest {
     static final boolean MAX_BOOLEAN = true;
     static final boolean MIN_BOOLEAN = false;
+    /**
+     * Tests {@link Arrays.toString(boolean[])}.
+     */
+    @Test
+    public void toStringBooleanArray() {
+        boolean[] blankBooleanArray = null;
+        boolean[] emptyBooleanArray = new boolean[] {};
+        boolean[] rangeBooleanArray = new boolean[] {MIN_BOOLEAN, MAX_BOOLEAN};
+        Assertions.assertTrue(edu.marywood.util.Arrays.toString(blankBooleanArray)
+                .equals(java.util.Arrays.toString(blankBooleanArray)));
+        Assertions.assertTrue(edu.marywood.util.Arrays.toString(emptyBooleanArray)
+                .equals(java.util.Arrays.toString(emptyBooleanArray)));
+        Assertions.assertTrue(edu.marywood.util.Arrays.toString(rangeBooleanArray)
+                .equals(java.util.Arrays.toString(rangeBooleanArray)));
+    }
 }
