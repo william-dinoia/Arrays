2d5c60c9eec9d7b039097803caaf68789f6b4e8e
William DiNoia <william@dinoia.email>
2026-09-09 19:14:58 -0400

    Create ArraysTest.toStringDoubleArray.

--- src/test/java/edu/marywood/util/ArraysTest.java
+++ src/test/java/edu/marywood/util/ArraysTest.java
@@ -62,4 +62,19 @@ public class ArraysTest {
         Assertions.assertTrue(
                 edu.marywood.util.Arrays.toString(rangeCharArray).equals(java.util.Arrays.toString(rangeCharArray)));
     }
+    /**
+     * Tests {@link Arrays.toString(double[])}.
+     */
+    @Test
+    public void toStringDoubleArray() {
+        double[] blankDoubleArray = null;
+        double[] emptyDoubleArray = new double[] {};
+        double[] rangeDoubleArray = new double[] {MIN_DOUBLE, MAX_DOUBLE};
+        Assertions.assertTrue(edu.marywood.util.Arrays.toString(blankDoubleArray)
+                .equals(java.util.Arrays.toString(blankDoubleArray)));
+        Assertions.assertTrue(edu.marywood.util.Arrays.toString(emptyDoubleArray)
+                .equals(java.util.Arrays.toString(emptyDoubleArray)));
+        Assertions.assertTrue(edu.marywood.util.Arrays.toString(rangeDoubleArray)
+                .equals(java.util.Arrays.toString(rangeDoubleArray)));
+    }
 }
