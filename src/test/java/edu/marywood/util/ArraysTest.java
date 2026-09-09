edea137a2c25416c96f31689ec6cbe36d4c96990
William DiNoia <william@dinoia.email>
2026-09-09 18:45:11 -0400

    Rename ArraysTest.MAX_CHARACTER, ArraysTest.MAX_CHAR.

--- src/test/java/edu/marywood/util/ArraysTest.java
+++ src/test/java/edu/marywood/util/ArraysTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 public class ArraysTest {
     static final boolean MAX_BOOLEAN = true;
     static final byte MAX_BYTE = Byte.MAX_VALUE;
-    static final char MAX_CHARACTER = Character.MAX_VALUE;
+    static final char MAX_CHAR = Character.MAX_VALUE;
     static final boolean MIN_BOOLEAN = false;
     static final byte MIN_BYTE = Byte.MIN_VALUE;
     static final char MIN_CHARACTER = Character.MIN_VALUE;
