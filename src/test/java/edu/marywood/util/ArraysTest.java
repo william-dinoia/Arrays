95f5043b26d0756fa66e167daf1d3c1d5e6a9eef
William DiNoia <william@dinoia.email>
2026-09-09 18:46:12 -0400

    Rename ArraysTest.MIN_CHARACTER, ArraysTest.MIN_CHAR.

--- src/test/java/edu/marywood/util/ArraysTest.java
+++ src/test/java/edu/marywood/util/ArraysTest.java
@@ -14,7 +14,7 @@ public class ArraysTest {
     static final char MAX_CHAR = Character.MAX_VALUE;
     static final boolean MIN_BOOLEAN = false;
     static final byte MIN_BYTE = Byte.MIN_VALUE;
-    static final char MIN_CHARACTER = Character.MIN_VALUE;
+    static final char MIN_CHAR = Character.MIN_VALUE;
     /**
      * Tests {@link Arrays.toString(boolean[])}.
      */
