43c2a340d4e2221e2dba86051ab43aff8203be34
William DiNoia <william@dinoia.email>
2026-09-12 18:27:15 -0400

    Create Benchmarks.

---
+++ src/main/java/edu/marywood/util/Benchmarks.java
@@ -0,0 +1,15 @@
+package edu.marywood.util;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * Defines {@link Benchmarks}.
+ *
+ * @author William DiNoia
+ */
+public class Benchmarks {
+}
