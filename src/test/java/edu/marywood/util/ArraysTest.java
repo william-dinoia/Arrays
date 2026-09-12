7aee4eb2820f95d0d1cbedf2df0c9ebe742279d8
William DiNoia <william@dinoia.email>
2026-09-12 10:29:38 -0400

    Modify ArraysTest.toStringIntArray.

--- src/test/java/edu/marywood/util/ArraysTest.java
+++ src/test/java/edu/marywood/util/ArraysTest.java
@@ -122,11 +122,14 @@ public class ArraysTest {
     public void toStringIntArray() {
         int[] blankIntArray = null;
         int[] emptyIntArray = new int[] {};
+        int[] lonerIntArray = new int[] {MIN_INT};
         int[] rangeIntArray = new int[] {MIN_INT, MAX_INT};
         Assertions.assertTrue(
                 edu.marywood.util.Arrays.toString(blankIntArray).equals(java.util.Arrays.toString(blankIntArray)));
         Assertions.assertTrue(
                 edu.marywood.util.Arrays.toString(emptyIntArray).equals(java.util.Arrays.toString(emptyIntArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(lonerIntArray).equals(java.util.Arrays.toString(lonerIntArray)));
         Assertions.assertTrue(
                 edu.marywood.util.Arrays.toString(rangeIntArray).equals(java.util.Arrays.toString(rangeIntArray)));
     }
