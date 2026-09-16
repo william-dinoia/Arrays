d16b6888a827078084878fad9346718c7838869b
William DiNoia <william@dinoia.email>
2026-09-16 16:27:42 -0400

    Format ArraysTest.

--- src/test/java/edu/marywood/util/ArraysTest.java
+++ src/test/java/edu/marywood/util/ArraysTest.java
@@ -9,27 +9,28 @@ import org.junit.jupiter.api.Test;
  * @author William DiNoia
  */
 public class ArraysTest {
-    /**
-     * Tests {@link Arrays.toString(boolean[])}.
-     */
+    /** Tests {@link Arrays.toString(boolean[])}. */
     @Test
     public void toStringBooleanArray() {
         boolean[] blankBooleanArray = null;
         boolean[] emptyBooleanArray = new boolean[] {};
         boolean[] lonerBooleanArray = new boolean[] {false};
         boolean[] rangeBooleanArray = new boolean[] {false, true};
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(blankBooleanArray)
-                .equals(java.util.Arrays.toString(blankBooleanArray)));
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(emptyBooleanArray)
-                .equals(java.util.Arrays.toString(emptyBooleanArray)));
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(lonerBooleanArray)
-                .equals(java.util.Arrays.toString(lonerBooleanArray)));
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(rangeBooleanArray)
-                .equals(java.util.Arrays.toString(rangeBooleanArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(blankBooleanArray)
+                        .equals(java.util.Arrays.toString(blankBooleanArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(emptyBooleanArray)
+                        .equals(java.util.Arrays.toString(emptyBooleanArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(lonerBooleanArray)
+                        .equals(java.util.Arrays.toString(lonerBooleanArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(rangeBooleanArray)
+                        .equals(java.util.Arrays.toString(rangeBooleanArray)));
     }
-    /**
-     * Tests {@link Arrays.toString(byte[])}.
-     */
+
+    /** Tests {@link Arrays.toString(byte[])}. */
     @Test
     public void toStringByteArray() {
         byte[] blankByteArray = null;
@@ -37,17 +38,20 @@ public class ArraysTest {
         byte[] lonerByteArray = new byte[] {Byte.MIN_VALUE};
         byte[] rangeByteArray = new byte[] {Byte.MIN_VALUE, Byte.MAX_VALUE};
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(blankByteArray).equals(java.util.Arrays.toString(blankByteArray)));
+                edu.marywood.util.Arrays.toString(blankByteArray)
+                        .equals(java.util.Arrays.toString(blankByteArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(emptyByteArray).equals(java.util.Arrays.toString(emptyByteArray)));
+                edu.marywood.util.Arrays.toString(emptyByteArray)
+                        .equals(java.util.Arrays.toString(emptyByteArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(lonerByteArray).equals(java.util.Arrays.toString(lonerByteArray)));
+                edu.marywood.util.Arrays.toString(lonerByteArray)
+                        .equals(java.util.Arrays.toString(lonerByteArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(rangeByteArray).equals(java.util.Arrays.toString(rangeByteArray)));
+                edu.marywood.util.Arrays.toString(rangeByteArray)
+                        .equals(java.util.Arrays.toString(rangeByteArray)));
     }
-    /**
-     * Tests {@link Arrays.toString(char[])}.
-     */
+
+    /** Tests {@link Arrays.toString(char[])}. */
     @Test
     public void toStringCharArray() {
         char[] blankCharArray = null;
@@ -55,35 +59,41 @@ public class ArraysTest {
         char[] lonerCharArray = new char[] {Character.MIN_VALUE};
         char[] rangeCharArray = new char[] {Character.MIN_VALUE, Character.MAX_VALUE};
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(blankCharArray).equals(java.util.Arrays.toString(blankCharArray)));
+                edu.marywood.util.Arrays.toString(blankCharArray)
+                        .equals(java.util.Arrays.toString(blankCharArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(emptyCharArray).equals(java.util.Arrays.toString(emptyCharArray)));
+                edu.marywood.util.Arrays.toString(emptyCharArray)
+                        .equals(java.util.Arrays.toString(emptyCharArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(lonerCharArray).equals(java.util.Arrays.toString(lonerCharArray)));
+                edu.marywood.util.Arrays.toString(lonerCharArray)
+                        .equals(java.util.Arrays.toString(lonerCharArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(rangeCharArray).equals(java.util.Arrays.toString(rangeCharArray)));
+                edu.marywood.util.Arrays.toString(rangeCharArray)
+                        .equals(java.util.Arrays.toString(rangeCharArray)));
     }
-    /**
-     * Tests {@link Arrays.toString(double[])}.
-     */
+
+    /** Tests {@link Arrays.toString(double[])}. */
     @Test
     public void toStringDoubleArray() {
         double[] blankDoubleArray = null;
         double[] emptyDoubleArray = new double[] {};
         double[] lonerDoubleArray = new double[] {Double.MIN_VALUE};
         double[] rangeDoubleArray = new double[] {Double.MIN_VALUE, Double.MAX_VALUE};
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(blankDoubleArray)
-                .equals(java.util.Arrays.toString(blankDoubleArray)));
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(emptyDoubleArray)
-                .equals(java.util.Arrays.toString(emptyDoubleArray)));
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(lonerDoubleArray)
-                .equals(java.util.Arrays.toString(lonerDoubleArray)));
-        Assertions.assertTrue(edu.marywood.util.Arrays.toString(rangeDoubleArray)
-                .equals(java.util.Arrays.toString(rangeDoubleArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(blankDoubleArray)
+                        .equals(java.util.Arrays.toString(blankDoubleArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(emptyDoubleArray)
+                        .equals(java.util.Arrays.toString(emptyDoubleArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(lonerDoubleArray)
+                        .equals(java.util.Arrays.toString(lonerDoubleArray)));
+        Assertions.assertTrue(
+                edu.marywood.util.Arrays.toString(rangeDoubleArray)
+                        .equals(java.util.Arrays.toString(rangeDoubleArray)));
     }
-    /**
-     * Tests {@link Arrays.toString(float[])}.
-     */
+
+    /** Tests {@link Arrays.toString(float[])}. */
     @Test
     public void toStringFloatArray() {
         float[] blankFloatArray = null;
@@ -91,17 +101,20 @@ public class ArraysTest {
         float[] lonerFloatArray = new float[] {Float.MIN_VALUE};
         float[] rangeFloatArray = new float[] {Float.MIN_VALUE, Float.MAX_VALUE};
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(blankFloatArray).equals(java.util.Arrays.toString(blankFloatArray)));
+                edu.marywood.util.Arrays.toString(blankFloatArray)
+                        .equals(java.util.Arrays.toString(blankFloatArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(emptyFloatArray).equals(java.util.Arrays.toString(emptyFloatArray)));
+                edu.marywood.util.Arrays.toString(emptyFloatArray)
+                        .equals(java.util.Arrays.toString(emptyFloatArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(lonerFloatArray).equals(java.util.Arrays.toString(lonerFloatArray)));
+                edu.marywood.util.Arrays.toString(lonerFloatArray)
+                        .equals(java.util.Arrays.toString(lonerFloatArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(rangeFloatArray).equals(java.util.Arrays.toString(rangeFloatArray)));
+                edu.marywood.util.Arrays.toString(rangeFloatArray)
+                        .equals(java.util.Arrays.toString(rangeFloatArray)));
     }
-    /**
-     * Tests {@link Arrays.toString(int[])}.
-     */
+
+    /** Tests {@link Arrays.toString(int[])}. */
     @Test
     public void toStringIntArray() {
         int[] blankIntArray = null;
@@ -109,17 +122,20 @@ public class ArraysTest {
         int[] lonerIntArray = new int[] {Integer.MIN_VALUE};
         int[] rangeIntArray = new int[] {Integer.MIN_VALUE, Integer.MAX_VALUE};
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(blankIntArray).equals(java.util.Arrays.toString(blankIntArray)));
+                edu.marywood.util.Arrays.toString(blankIntArray)
+                        .equals(java.util.Arrays.toString(blankIntArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(emptyIntArray).equals(java.util.Arrays.toString(emptyIntArray)));
+                edu.marywood.util.Arrays.toString(emptyIntArray)
+                        .equals(java.util.Arrays.toString(emptyIntArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(lonerIntArray).equals(java.util.Arrays.toString(lonerIntArray)));
+                edu.marywood.util.Arrays.toString(lonerIntArray)
+                        .equals(java.util.Arrays.toString(lonerIntArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(rangeIntArray).equals(java.util.Arrays.toString(rangeIntArray)));
+                edu.marywood.util.Arrays.toString(rangeIntArray)
+                        .equals(java.util.Arrays.toString(rangeIntArray)));
     }
-    /**
-     * Tests {@link Arrays.toString(long[])}.
-     */
+
+    /** Tests {@link Arrays.toString(long[])}. */
     @Test
     public void toStringLongArray() {
         long[] blankLongArray = null;
@@ -127,17 +143,20 @@ public class ArraysTest {
         long[] lonerLongArray = new long[] {Long.MIN_VALUE};
         long[] rangeLongArray = new long[] {Long.MIN_VALUE, Long.MAX_VALUE};
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(blankLongArray).equals(java.util.Arrays.toString(blankLongArray)));
+                edu.marywood.util.Arrays.toString(blankLongArray)
+                        .equals(java.util.Arrays.toString(blankLongArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(emptyLongArray).equals(java.util.Arrays.toString(emptyLongArray)));
+                edu.marywood.util.Arrays.toString(emptyLongArray)
+                        .equals(java.util.Arrays.toString(emptyLongArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(lonerLongArray).equals(java.util.Arrays.toString(lonerLongArray)));
+                edu.marywood.util.Arrays.toString(lonerLongArray)
+                        .equals(java.util.Arrays.toString(lonerLongArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(rangeLongArray).equals(java.util.Arrays.toString(rangeLongArray)));
+                edu.marywood.util.Arrays.toString(rangeLongArray)
+                        .equals(java.util.Arrays.toString(rangeLongArray)));
     }
-    /**
-     * Tests {@link Arrays.toString(short[])}.
-     */
+
+    /** Tests {@link Arrays.toString(short[])}. */
     @Test
     public void toStringShortArray() {
         short[] blankShortArray = null;
@@ -145,12 +164,16 @@ public class ArraysTest {
         short[] lonerShortArray = new short[] {Short.MIN_VALUE};
         short[] rangeShortArray = new short[] {Short.MIN_VALUE, Short.MAX_VALUE};
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(blankShortArray).equals(java.util.Arrays.toString(blankShortArray)));
+                edu.marywood.util.Arrays.toString(blankShortArray)
+                        .equals(java.util.Arrays.toString(blankShortArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(emptyShortArray).equals(java.util.Arrays.toString(emptyShortArray)));
+                edu.marywood.util.Arrays.toString(emptyShortArray)
+                        .equals(java.util.Arrays.toString(emptyShortArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(lonerShortArray).equals(java.util.Arrays.toString(lonerShortArray)));
+                edu.marywood.util.Arrays.toString(lonerShortArray)
+                        .equals(java.util.Arrays.toString(lonerShortArray)));
         Assertions.assertTrue(
-                edu.marywood.util.Arrays.toString(rangeShortArray).equals(java.util.Arrays.toString(rangeShortArray)));
+                edu.marywood.util.Arrays.toString(rangeShortArray)
+                        .equals(java.util.Arrays.toString(rangeShortArray)));
     }
 }
