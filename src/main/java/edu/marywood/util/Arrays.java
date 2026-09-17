c20c541abf775f24a8c9a11e2c085be4daa00dbb
William DiNoia <william@dinoia.email>
2026-09-17 12:40:40 -0400

    Modify Arrays.toString(short[]).

--- src/main/java/edu/marywood/util/Arrays.java
+++ src/main/java/edu/marywood/util/Arrays.java
@@ -224,7 +224,8 @@ public class Arrays {
         if (shortArray == null) {
             return NULL;
         } else {
-            StringBuilder stringBuilder = new StringBuilder().append(LEFT_SQUARE_BRACKET);
+            StringBuilder stringBuilder =
+                    new StringBuilder(shortArray.length).append(LEFT_SQUARE_BRACKET);
             switch (shortArray.length) {
                 case 0:
                     break;
