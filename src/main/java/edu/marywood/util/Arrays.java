6c67dcb4ed5d275f116d64c04d063f2e958186e7
William DiNoia <william@dinoia.email>
2026-09-19 16:50:31 -0400

    Modify Arrays.toString(float[]).

--- src/main/java/edu/marywood/util/Arrays.java
+++ src/main/java/edu/marywood/util/Arrays.java
@@ -145,11 +145,14 @@ public class Arrays {
             return EMPTY_ARRAY;
         } else {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(LEFT_SQUARE_BRACKET).append(floatArray[0]);
+            stringBuilder.append(LEFT_SQUARE_BRACKET);
+            stringBuilder.append(floatArray[0]);
             for (int index = 1; index < floatArray.length; index += 1) {
-                stringBuilder.append(COMMA).append(SPACE).append(floatArray[index]);
+                stringBuilder.append(DELIMITER);
+                stringBuilder.append(floatArray[index]);
             }
-            return stringBuilder.append(RIGHT_SQUARE_BRACKET).toString();
+            stringBuilder.append(RIGHT_SQUARE_BRACKET);
+            return stringBuilder.toString();
         }
     }
 
