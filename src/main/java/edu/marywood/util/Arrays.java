68fc3472e388eae4732bd649dbc3f23fc9427875
William DiNoia <william@dinoia.email>
2026-09-19 17:09:28 -0400

    Modify Arrays.toString(short[]).

--- src/main/java/edu/marywood/util/Arrays.java
+++ src/main/java/edu/marywood/util/Arrays.java
@@ -217,11 +217,14 @@ public class Arrays {
             return EMPTY_ARRAY;
         } else {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(LEFT_SQUARE_BRACKET).append(shortArray[0]);
+            stringBuilder.append(LEFT_SQUARE_BRACKET);
+            stringBuilder.append(shortArray[0]);
             for (int index = 1; index < shortArray.length; index += 1) {
-                stringBuilder.append(COMMA).append(SPACE).append(shortArray[index]);
+                stringBuilder.append(DELIMITER);
+                stringBuilder.append(shortArray[index]);
             }
-            return stringBuilder.append(RIGHT_SQUARE_BRACKET).toString();
+            stringBuilder.append(RIGHT_SQUARE_BRACKET);
+            return stringBuilder.toString();
         }
     }
 }
