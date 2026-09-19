28cc862a3dbab8d01e71b31d926b99105e2c255f
William DiNoia <william@dinoia.email>
2026-09-19 16:31:05 -0400

    Create Arrays.DELIMITER.

--- src/main/java/edu/marywood/util/Arrays.java
+++ src/main/java/edu/marywood/util/Arrays.java
@@ -7,6 +7,7 @@ package edu.marywood.util;
  */
 public class Arrays {
     private static final char COMMA = ',';
+    private static final String DELIMITER = ", ";
     private static final String EMPTY_ARRAY = "[]";
     private static final char LEFT_SQUARE_BRACKET = '[';
     private static final String NULL = "null";
