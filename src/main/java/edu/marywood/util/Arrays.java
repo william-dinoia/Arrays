d7c6c5f01c7c898363c33d863cff0410e81742e0
William DiNoia <william@dinoia.email>
2026-09-21 16:44:44 -0400

    Modify Arrays.toString(int[]).

--- src/main/java/edu/marywood/util/Arrays.java
+++ src/main/java/edu/marywood/util/Arrays.java
@@ -193,6 +193,7 @@ public class Arrays {
             return "[]";
         } else {
             StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.ensureCapacity(3 * intArray.length);
             stringBuilder.append('[');
             stringBuilder.append(intArray[0]);
             for (int index = 1; index < intArray.length; index += 1) {
