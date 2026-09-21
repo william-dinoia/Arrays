8901b55dc371fdacbfd355a4074691d5c65b5500
William DiNoia <william@dinoia.email>
2026-09-21 16:18:32 -0400

    Modify Arrays.toString(boolean[]).

--- src/main/java/edu/marywood/util/Arrays.java
+++ src/main/java/edu/marywood/util/Arrays.java
@@ -68,6 +68,7 @@ public class Arrays {
             return "[]";
         } else {
             StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.ensureCapacity(6 * booleanArray.length);
             stringBuilder.append('[');
             stringBuilder.append(booleanArray[0]);
             for (int index = 1; index < booleanArray.length; index += 1) {
